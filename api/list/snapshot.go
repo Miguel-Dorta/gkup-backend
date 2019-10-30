e33ee62dd60da5ec32975290c318f4e708e88555
Miguel-Dorta <contact@migueldorta.com>
2019-10-30 21:21:34 +0000

    Comment list api

--- api/list/snapshot.go
+++ api/list/snapshot.go
@@ -1,14 +1,17 @@
 package list
 
+// SnapshotList represents a serializable list of Snapshot objects.
 type SnapshotList struct {
 	SList []*Snapshot `json:"snapshots"`
 }
 
+// Snapshot represents all the snapshots taken under the same name.
 type Snapshot struct {
 	Name  string  `json:"name"`
 	Times []*Time `json:"times"`
 }
 
+// Time represents the time when a snapshot was taken.
 type Time struct {
 	Year   int `json:"year"`
 	Month  int `json:"month"`
