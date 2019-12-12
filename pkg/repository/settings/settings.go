2137e33c69df67641e46c2e28b1abff861c44861
Miguel-Dorta <contact@migueldorta.com>
2019-12-12 16:14:14 +0000

    Add db info to settings

--- pkg/repository/settings/settings.go
+++ pkg/repository/settings/settings.go
@@ -3,61 +3,60 @@ package settings
 import (
 	"errors"
 	"fmt"
-	"github.com/Miguel-Dorta/gkup-backend/internal"
 	"github.com/Miguel-Dorta/gkup-backend/pkg"
 	"github.com/pelletier/go-toml"
 	"io/ioutil"
-	"os"
 )
+
+//TODO add new stuff to tests and comment
 
 const FileName = "settings.toml"
 
 type Settings struct {
-	Version string `toml:"version"`
+	Version       string `toml:"version"`
 	HashAlgorithm string `toml:"hash_algorithm"`
+	SnapshotType  string `toml:"snapshot_type"`
+	DB            DB     `toml:"database"`
 }
 
-func Read(path string) (Settings, error) {
+type DB struct {
+	Host   string `toml:"hostname"`
+	DBName string `toml:"database_name"`
+	User   string `toml:"username"`
+	Pass   string `toml:"password"`
+	Port   int    `toml:"port"`
+}
+
+func Read(path string) (*Settings, error) {
 	// Read data
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		return Settings{}, &os.PathError{
-			Op: "read settings",
-			Path: path,
-			Err: err,
-		}
+		return nil, fmt.Errorf("error reading setting in path \"%s\": %s", path, err)
 	}
 
 	// Parse settings
-	s := Settings{}
+	s := new(Settings)
 	if err := toml.Unmarshal(data, &s); err != nil {
-		return Settings{}, fmt.Errorf("error parsing settings: %s", err)
+		return nil, fmt.Errorf("error parsing settings: %s", err)
 	}
 
 	// Check if the fields have info
-	if s.Version == "" || s.HashAlgorithm == "" {
-		return Settings{}, errors.New("incomplete information in settings")
+	if s.Version == "" || s.HashAlgorithm == "" || s.SnapshotType == "" {
+		return nil, errors.New("incomplete information in settings")
 	}
 	return s, nil
 }
 
-func Write(path, hashAlgorithm string) error {
+func Write(path string, s *Settings) error {
 	// Serialize settings
-	data, err := toml.Marshal(&Settings{
-		Version:       internal.Version,
-		HashAlgorithm: hashAlgorithm,
-	})
+	data, err := toml.Marshal(s)
 	if err != nil {
 		return fmt.Errorf("error serializing settings: %s", err)
 	}
 
 	// Write data
 	if err := ioutil.WriteFile(path, data, pkg.DefaultFilePerm); err != nil {
-		return &os.PathError{
-			Op:   "write settings",
-			Path: path,
-			Err:  err,
-		}
+		return fmt.Errorf("error writing settings in path \"%s\": %s", path, err)
 	}
 	return nil
 }
