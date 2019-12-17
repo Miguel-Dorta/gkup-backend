95aa7c56e2a2c403f3ef8a5db43a96a5e67ae5b0
Miguel-Dorta <contact@migueldorta.com>
2019-12-17 13:25:34 +0000

    Implement hasher with settings

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -6,33 +6,53 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"fmt"
+	"github.com/Miguel-Dorta/gkup-backend/pkg/repository/settings"
 	"golang.org/x/crypto/sha3"
 	"hash"
 	"io"
 	"os"
 )
 
-var (
-	Algorithms = map[string]func() hash.Hash{
-		"md5":      md5.New,
-		"sha1":     sha1.New,
-		"sha256":   sha256.New,
-		"sha512":   sha512.New,
-		"sha3-256": sha3.New256,
-		"sha3-512": sha3.New512,
-	}
-)
+type Hasher struct {
+	h   hash.Hash
+	buf []byte
+}
 
-func HashFile(path string, h hash.Hash, buf []byte) ([]byte, error) {
+func NewHasher(s *settings.Settings) (*Hasher, error) {
+	hasher := &Hasher{
+		buf: make([]byte, s.BufferSize),
+	}
+
+	switch s.HashAlgorithm {
+	case "md5":
+		hasher.h = md5.New()
+	case "sha1":
+		hasher.h = sha1.New()
+	case "sha256":
+		hasher.h = sha256.New()
+	case "sha512":
+		hasher.h = sha512.New()
+	case "sha3-256":
+		hasher.h = sha3.New256()
+	case "sha3-512":
+		hasher.h = sha3.New512()
+	default:
+		return nil, fmt.Errorf("invalid hash algorithm: %s", s.HashAlgorithm)
+	}
+
+	return hasher, nil
+}
+
+func (h *Hasher) HashFile(path string) ([]byte, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return nil, fmt.Errorf("cannot open file (%s): %w", path, err)
 	}
 	defer f.Close()
 
-	h.Reset()
-	if _, err = io.CopyBuffer(h, f, buf); err != nil {
+	h.h.Reset()
+	if _, err = io.CopyBuffer(h.h, f, h.buf); err != nil {
 		return nil, fmt.Errorf("error hashing file (%s): %w", path, err)
 	}
-	return h.Sum(nil), err
+	return h.h.Sum(nil), err
 }
