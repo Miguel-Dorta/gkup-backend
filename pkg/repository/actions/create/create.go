e79e23425423bfc3d04e00bb714059d25d383d2a
Miguel-Dorta <contact@migueldorta.com>
2020-01-10 17:52:06 +0000

    Divide create.Create in functions and create files folder

--- pkg/repository/actions/create/create.go
+++ pkg/repository/actions/create/create.go
@@ -1,44 +1,28 @@
 package create
 
 import (
+	"encoding/hex"
 	"fmt"
-	"github.com/Miguel-Dorta/gkup-backend/pkg/fileutils"
+	"github.com/Miguel-Dorta/gkup-backend/pkg/repository/files"
 	"github.com/Miguel-Dorta/gkup-backend/pkg/repository/settings"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
 
 // TODO check tests
-// TODO create folders and params!
 
 func Create(path string, s *settings.Settings, errWriter io.Writer) {
-	// Check existence
-	if exists, err := fileutils.FileExist(path); err != nil {
-		printError(errWriter, "error checking existence of path \"%s\": %s", path, err)
-		return
-	} else if !exists {
-		if err := os.MkdirAll(path, 0755); err != nil {
-			printError(errWriter, "error creating repo directory in path \"%s\": %s", path, err)
-			return
-		}
-	}
-
-	// Check if it's not a dir
-	if stat, err := os.Stat(path); err != nil {
-		printError(errWriter, "cannot get information from path \"%s\": %s", path, err)
-		return
-	} else if !stat.IsDir() {
-		printError(errWriter, "repository path must be a directory")
+	// Check repo path
+	if err := checkRepoPath(path); err != nil {
+		printError(errWriter, err.Error())
 		return
 	}
 
-	// Check if dir is empty
-	if list, err := fileutils.ListDir(path); err != nil {
-		printError(errWriter, "error listing repository directory (%s): %s\n", path, err)
-		return
-	} else if len(list) != 0 {
-		printError(errWriter, "repository path must be empty")
+	// Create files directories
+	if err := createFilesDirs(path); err != nil {
+		printError(errWriter, err.Error())
 		return
 	}
 
@@ -50,6 +34,49 @@ func Create(path string, s *settings.Settings, errWriter io.Writer) {
 	}
 }
 
+func checkRepoPath(path string) error {
+	stat, err := os.Stat(path)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return fmt.Errorf("error getting repository path (%s) info: %s", path, err)
+		}
+
+		// If the error is that path doesn't exists, create it
+		if err := os.MkdirAll(path, 0755); err != nil {
+			return fmt.Errorf("error creating repository directory (%s): %s", path, err)
+		}
+		return nil
+	}
+
+	if !stat.IsDir() {
+		return fmt.Errorf("repository path (%s) is not a directory", path)
+	}
+
+	fs, err := ioutil.ReadDir(path)
+	if err != nil {
+		return fmt.Errorf("error listing repository path (%s) content: %s", path, err)
+	}
+	if len(fs) != 0 {
+		return fmt.Errorf("repository path (%s) must be empty", path)
+	}
+	return nil
+}
+
+func createFilesDirs(path string) error {
+	path = filepath.Join(path, files.FolderName)
+	for i:=0x0; i<=0xff; i++ {
+		iPath := filepath.Join(path, hex.EncodeToString([]byte{byte(i)}))
+		if err := os.MkdirAll(iPath, 0755); err != nil {
+			return fmt.Errorf("error creating directory \"%s\": %s", iPath, err)
+		}
+	}
+	return nil
+}
+
 func printError(w io.Writer, format string, a ...interface{}) {
+	if a == nil {
+		_, _ = fmt.Fprintln(w, format)
+		return
+	}
 	_, _ = fmt.Fprintf(w, format+"\n", a...)
 }
